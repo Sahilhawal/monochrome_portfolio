47f13f383d583bc136dc5fced534ff9b62dc810e
Sahil Hawal <sahilhawal1996@gmail.com>
2024-01-29 09:37:28 +0530

    fixed build issues

---
+++ app/components/intro.tsx
@@ -0,0 +1,42 @@
+"use client";
+// pages/index.js
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Intro() {
+  const [activeTab, setActiveTab] = useState("fitpage");
+
+  return (
+    <section className="bg-black h-screen flex flex-col md:flex-row-reverse justify-between ">
+      <div className="w-full h-1/2 flex items-center justify-center m-auto">
+        <div>
+          <h1 className="text-5xl mb:text-9xl font-bold">Sahil Hawal</h1>
+          <p className="text-xl mb:text-4xl text-blue-500">Software Engineer</p>
+          <p className="text-lg max-w-2xl mt-4">
+            Hi! I am a software engineer with 4.5+ years of experience in
+            developing web applications and backend systems. I enjoy planning
+            and building things, and I am always up for exploring new
+            technologies.
+          </p>
+        </div>
+      </div>
+
+      <div className="relative w-full h-[300px] md:h-[500px] md:self-end">
+        <div className="absolute top-2 left-1/4 md:left-1/2 md:top-10">
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            <p className="text-gray-800">{"btw, I use vim"}</p>
+          </div>
+          <div className="w-0 h-0 border-t-8 border-transparent border-solid border-white"></div>
+        </div>
+        <Image
+          width={204}
+          height={250}
+          src="/sahil.png"
+          alt="Sahil Hawal"
+          className="absolute bottom-0 left-0 w-auto md:h-full object-contain"
+        />
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@
 // pages/index.js
 import Head from "next/head";
 import { useState } from "react";
-
+import Intro from "./components/intro";
+import {
+  FaMapMarkerAlt,
+  FaPhone,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
 export default function Home() {
   const [activeSection, setActiveSection] = useState("home");
   const [activeTab, setActiveTab] = useState("fitpage");
-  const scrollToSection = (section) => {
+  const [activeSkillSection, setActiveSkillSection] = useState("languages");
+  const scrollToSection = (section: any) => {
     const element = document.getElementById(section);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
@@ -14,7 +22,14 @@ export default function Home() {
     }
   };
 
-  const experienceData = {
+  const scrollToSkillSection = (section: any) => {
+    setActiveSkillSection(section);
+  };
+
+  const experienceData: Record<
+    string,
+    { title: string; date: string; points: string[] }
+  > = {
     fitpage: {
       title: "Senior Application Developer | Fitpage, BKC",
       date: "March 2022 - Present",
@@ -60,7 +75,7 @@ export default function Home() {
           }`}
           onClick={() => scrollToSection("home")}
         >
-          Home
+          /Home
         </a>
         <a
           className={`mx-4 cursor-pointer ${
@@ -68,7 +83,7 @@ export default function Home() {
           }`}
           onClick={() => scrollToSection("experience")}
         >
-          Experience
+          /Experience
         </a>
         <a
           className={`mx-4 cursor-pointer ${
@@ -76,7 +91,7 @@ export default function Home() {
           }`}
           onClick={() => scrollToSection("skills")}
         >
-          Skills
+          /Skills
         </a>
         <a
           className={`mx-4 cursor-pointer ${
@@ -84,7 +99,7 @@ export default function Home() {
           }`}
           onClick={() => scrollToSection("projects")}
         >
-          Projects
+          /Projects
         </a>
         <a
           className={`mx-4 cursor-pointer ${
@@ -92,38 +107,17 @@ export default function Home() {
           }`}
           onClick={() => scrollToSection("contact")}
         >
-          Contact
+          /Contact
         </a>
       </nav>
-
-      <section className="bg-black h-screen flex items-center justify-around relative">
-        <div className="relative w-1/2 h-full">
-          <img
-            src="/sahil.png"
-            alt="Sahil Hawal"
-            className="absolute bottom-0"
-          />
-        </div>
-        <div className="w-1/2 flex items-center justify-center">
-          <div>
-            <h1 className="text-9xl font-bold">Sahil Hawal</h1>
-            <p className="text-4xl">Software Engineer</p>
-            <p className="text-lg max-w-2xl mt-4">
-              Hi! I'm a software engineer with 4.5+ years of experience in
-              developing web applications and backend systems. I enjoy planning
-              and building things, and I'm always up for exploring new
-              technologies.
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className="mx-auto my-20 h-[500px] container flex items-center">
-        <div className="w-full">
+      <main className="max-w-[360px] lg:max-w-[900px] 2xl:max-w-[1200px] mx-auto">
+        <Intro />
+        <section className="mx-auto my-20 flex flex-col justify-center">
           {/* Vertical Tabs on Left */}
           <h2 className="text-2xl border-b-2 border-white pb-2 mb-4">
             Experience
           </h2>
-          <div className="flex my-9">
+          <div className="flex my-9 h-[200px]">
             <div className="flex flex-col mr-8 gap-4">
               <button
                 className={`text-lg mb-4 focus:outline-none ${
@@ -158,53 +152,174 @@ export default function Home() {
               </h3>
               <p className="text-gray-400">{experienceData[activeTab].date}</p>
               <ul className="list-disc pl-6">
-                {experienceData[activeTab].points.map((point, index) => (
-                  <li key={index}>{point}</li>
-                ))}
+                {experienceData[activeTab].points.map(
+                  (point: any, index: any) => (
+                    <li key={index}>{point}</li>
+                  )
+                )}
               </ul>
             </div>
           </div>
+        </section>
+        <section className="mx-auto my-20 flex flex-col justify-center  lg:max-w-[900px] 2xl:max-w-[1200px]">
+          <h2 className="text-2xl border-b-2 border-white pb-2 mb-4">Skills</h2>
+
+          {/* Skill categories */}
+          <div className="mb-6 flex">
+            {["languages", "frameworks", "databases", "tools"].map(
+              (category) => (
+                <button
+                  key={category}
+                  className={`text-lg mx-4 focus:outline-none ${
+                    activeSkillSection === category &&
+                    "border-b-2 border-blue-500"
+                  }`}
+                  onClick={() => scrollToSkillSection(category)}
+                >
+                  {category.charAt(0).toUpperCase() + category.slice(1)}
+                </button>
+              )
+            )}
+          </div>
+
+          {/* Skill content based on active skill section */}
+          <div className="h-[200px]">
+            {activeSkillSection === "languages" && (
+              <ul className="list-disc pl-6">
+                <li>JavaScript, TypeScript</li>
+                <li>HTML, CSS</li>
+                <li>Ruby</li>
+                <li>Python</li>
+              </ul>
+            )}
+
+            {activeSkillSection === "frameworks" && (
+              <ul className="list-disc pl-6">
+                <li>React</li>
+                <li>Next.js, Redux</li>
+                <li>Nest.js, Tailwind</li>
+                <li>Ruby on Rails</li>
+              </ul>
+            )}
+
+            {activeSkillSection === "databases" && (
+              <ul className="list-disc pl-6">
+                <li>SQL (PostgreSQL)</li>
+              </ul>
+            )}
+
+            {activeSkillSection === "tools" && (
+              <ul className="list-disc pl-6">
+                <li>AWS Rekognition</li>
+                <li>Kafka</li>
+                <li>Bull Queue</li>
+                <li>Docker</li>
+                <li>Vim, Tmux, Git</li>
+              </ul>
+            )}
+          </div>
+        </section>
+        {/* <section className="container mx-auto mb-8">
+          <h2 className="text-2xl border-b-2 border-white pb-2 mb-4">
+            Tech that I have worked extensively on
+          </h2>
+
+          <div className="mb-6">
+            <h3 className="text-xl font-semibold">React Hook Form</h3>
+            <p>
+              A simple neovim distribution to quickly set up an IDE-like
+              experience in your terminal.
+            </p>
+            <p className="text-gray-400">
+              <a href="your_kiss_vim_project_link">Link</a>
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-xl font-semibold">Kafka</h3>
+            <p>A minimal, dark theme for VS Code.</p>
+            <p className="text-gray-400">
+              <a href="your_everything_zen_project_link">Link</a>
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-xl font-semibold">Image Processing</h3>
+            <p>A minimal, dark theme for VS Code.</p>
+            <p className="text-gray-400">
+              <a href="your_everything_zen_project_link">Link</a>
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-xl font-semibold">Code Editor</h3>
+            <p>A minimal, dark theme for VS Code.</p>
+            <p className="text-gray-400">
+              <a href="your_everything_zen_project_link">Link</a>
+            </p>
+          </div>
+        </section> */}
+
+        <section className="mx-auto my-20 flex flex-col">
+          <h2 className="text-2xl border-b-2 border-white pb-2 mb-4">
+            Projects
+          </h2>
+
+          <div className="mb-6">
+            <h3 className="text-xl font-semibold">Kiss-vim</h3>
+            <p>
+              A simple neovim distribution to quickly set up an IDE-like
+              experience in your terminal.
+            </p>
+            <p className="text-gray-400">
+              <a href="your_kiss_vim_project_link">Link</a>
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-xl font-semibold">Everything-zen</h3>
+            <p>A minimal, dark theme for VS Code.</p>
+            <p className="text-gray-400">
+              <a href="your_everything_zen_project_link">Link</a>
+            </p>
+          </div>
+        </section>
+      </main>
+      <div className="fixed left-8 bottom-44">
+        <div className="flex flex-col space-y-4">
+          {/* Email */}
+          <div className="flex items-center">
+            <a href="mailto:sahilhawal1996@gmail.com">
+              <FaEnvelope className="mr-2 text-white" />
+              {/* <p>sahilhawal1996@gmail.com</p> */}
+            </a>
+          </div>
+          {/* GitHub */}
+          <div className="flex items-center">
+            <a
+              href="https://github.com/sahilhawal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="mr-2 text-white" />
+              {/* <p>sahilhawal</p> */}
+            </a>
+          </div>
+          {/* LinkedIn */}
+          <div className="flex items-center">
+            <a
+              href="https://www.linkedin.com/in/sahil-hawal/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="mr-2 text-white" />
+              {/* <p>sahil-hawal</p> */}
+            </a>
+          </div>
         </div>
-      </section>
+      </div>
 
-      <section className="container mx-auto mb-8">
-        <h2 className="text-2xl border-b-2 border-white pb-2 mb-4">Projects</h2>
-
-        <div className="mb-6">
-          <h3 className="text-xl font-semibold">Kiss-vim</h3>
-          <p>
-            A simple neovim distribution to quickly set up an IDE-like
-            experience in your terminal.
-          </p>
-          <p className="text-gray-400">
-            <a href="your_kiss_vim_project_link">Link</a>
-          </p>
-        </div>
-
-        <div>
-          <h3 className="text-xl font-semibold">Everything-zen</h3>
-          <p>A minimal, dark theme for VS Code.</p>
-          <p className="text-gray-400">
-            <a href="your_everything_zen_project_link">Link</a>
-          </p>
-        </div>
-      </section>
-
-      <section className="container mx-auto mb-8">
-        <h2 className="text-2xl border-b-2 border-white pb-2 mb-4">Projects</h2>
-        {/* Add your project details here */}
-      </section>
-
-      <section className="container mx-auto mb-8">
-        <h2 className="text-2xl border-b-2 border-white pb-2 mb-4">Contact</h2>
-        <p>Navi Mumbai, MH India</p>
-        <p>+91 7666046688</p>
-        <p>Email: sahilhawal1996@gmail.com</p>
-        <p>Github: sahilhawal</p>
-        <p>LinkedIn: sahil-hawal</p>
-      </section>
-
-      <footer className="text-center py-4 bg-gray-800">
+      <footer className="text-center py-4 bg-white text-black">
         <p>&copy; 2024 Sahil Hawal. All Rights Reserved.</p>
       </footer>
     </div>
